3355363d538c08536b73dbd1d1635fc632bd3714
Cedric <cedric@lphn.fr>
2022-06-29 21:28:42 +0200

    feat: debut opti md5

--- src/md5cash_challenge.rs
+++ src/md5cash_challenge.rs
@@ -29,27 +29,50 @@ impl Challenge for HashCash {
 
     fn solve(&self) -> MD5HashCashOutput {
         let threads: usize = num_cpus::get();
-
-        println!("{:}", num_cpus::get());
         let (tx, rx): (Sender<MD5HashCashOutput>, Receiver<MD5HashCashOutput>) = mpsc::channel();
         let mut i : u64 = 0;
-        loop {
-            if start_challenge(i, self.input.complexity.clone(), self.input.message.clone(), tx.clone()) {
-                break;
-            }
-            i += 1;
+
+
+        for th in 0..threads {
+            let tx = tx.clone();
+            let complexity = self.input.complexity.clone();
+            let message = self.input.message.clone();
+            thread::spawn(move || {
+                loop {
+                    let hash = format!("{:016X}", i) + &message;
+
+                    let result = format!("{:x}", md5::compute(hash));
+
+                    let binary   = u128::from_str_radix(&*result, 16).unwrap();
+                    let mut check = format!("{:0128b}", binary);
+                    let check2 : Vec<&str> = check.split("1").collect();
+                    let sum : u32 = check2[0].len() as u32;
+                    if sum == complexity {
+                        let result : MD5HashCashOutput = MD5HashCashOutput {
+                            seed: i as u64,
+                            hashcode : result
+                        };
+                        tx.send(result).unwrap();
+
+                    }
+                    i += 1;
+
+                }
+
+            });
         }
 
+
+
         for message in rx.iter().take(threads) {
-            if message.hashcode != "ERR" {
-                return message;
-            }
+            return message;
         }
 
         return MD5HashCashOutput {
             seed : 0,
             hashcode: "ERR".parse().unwrap()
         }
+
     }
 
     fn verify(&self, answer: Self::Output) -> bool {
@@ -59,40 +82,4 @@ impl Challenge for HashCash {
 
 trait CommandOutput {
     fn get_command_output() -> Result<(), ()>;
-}
-
-fn start_challenge(i : u64, complexity: u32, message : String, tx : Sender<MD5HashCashOutput>) -> bool {
-    println!("Testing with {:}", i);
-
-    let hash = format!("{:016X}", i) + &message;
-    print!("{:?} = ", hash);
-
-    let result = format!("{:x}", md5::compute(hash));
-    print!("{:?} ", result);
-
-    let binary   = u128::from_str_radix(&*result, 16).unwrap();
-    let mut check = format!("{:0128b}", binary);
-    let check2 : Vec<&str> = check.split("1").collect();
-    let sum : u32 = check2[0].len() as u32;
-    return if sum == complexity {
-        println!("Checksum match !");
-        let result: MD5HashCashOutput = MD5HashCashOutput {
-            seed: i as u64,
-            hashcode: result
-        };
-        tx.send(result).unwrap();
-        true
-    } else {
-        println!("ERR");
-        false
-    }
-}
-
-fn main() {
-    let input = MD5HashCashInput {
-        complexity: 16,
-        message: String::from("When i was child, i killed a boar at close range")
-    };
-    let test = HashCash::new(input);
-    println!("{:?}", test.solve());
 }
